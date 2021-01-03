13aa553fd25c8adbfae295e1ea416eb4610adaaa
Jacob Mura <jacobaraujo7@gmail.com>
2021-01-03 00:50:18 -0300

    added extesnionx

--- lib/extensions/rx_extensions.dart
+++ lib/extensions/rx_extensions.dart
@@ -5,3 +5,23 @@ import '../rx_notifier.dart';
 extension ValueNotifierParse<T> on ValueListenable<T> {
   RxNotifier<T> asRx() => RxNotifier<T>(value);
 }
+
+extension RxStreamExtension<T> on Stream<T> {
+  RxStream<T> asRx() => RxStream.of<T>(this);
+}
+
+extension RxFutureExtension<T> on Future<T> {
+  RxFuture<T> asRx() => RxFuture.of<T>(this);
+}
+
+extension RxListExtension<T> on List<T> {
+  RxList<T> asRx() => RxList.of<T>(this);
+}
+
+extension RxSetExtension<T> on Set<T> {
+  RxSet<T> asRx() => RxSet.of<T>(this);
+}
+
+extension RxMapExtension<K, V> on Map<K, V> {
+  RxMap<K, V> asRx() => RxMap.of<K, V>(this);
+}
