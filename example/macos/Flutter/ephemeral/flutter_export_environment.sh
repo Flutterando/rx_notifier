c8d60519ddc5c1f25078d60feb4815f44f85a532
Jacob Moura <jacobaraujo7@gmail.com>
2021-01-05 15:01:47 -0300

    added example

---
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jacobmoura/fvm/versions/master"
+export "FLUTTER_APPLICATION_PATH=/Users/jacobmoura/Projects/rx_notifier/example"
+export "FLUTTER_TARGET=/Users/jacobmoura/Projects/rx_notifier/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jacobmoura/Projects/rx_notifier/example/.dart_tool/package_config.json"
